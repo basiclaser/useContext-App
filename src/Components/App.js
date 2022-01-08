8f47d62329779523fe9588c37010ecea0108894c
z-memory <dont@forgetmy.email>
2022-01-08 15:16:04 +0100

    add luxon, .env, ipify examples

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,9 +2,19 @@ import { useEffect, useState, useContext } from "react";
 import { UserContext } from "../Contexts/UserContext";
 import "./App.css";
 import Navbar from "./Navbar";
+import { DateTime } from "luxon";
 
 function App() {
   const { user } = useContext(UserContext);
+
+  const [userGeoIpData, setUserGeoIpData] = useState(null);
+  useEffect(() => {
+    fetch(
+      `https://geo.ipify.org/api/v2/country?apiKey=${process.env.REACT_APP_IPIFY_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => setUserGeoIpData(data));
+  }, []);
 
   useEffect(() => {
     console.log(user);
@@ -12,7 +22,12 @@ function App() {
 
   return (
     <div className="App component">
-      <h1>PROP DRILLING & useContext & provider pattern</h1>
+      <h1>hi {userGeoIpData && userGeoIpData.ip}</h1>
+      {process.env.REACT_APP_TEST_ABC}
+      {DateTime.local()
+        .setLocale("de")
+        .plus({ days: -7 })
+        .toRelative({ unit: "minutes" })}
       {user?.picture?.thumbnail ? <Navbar /> : "loading"}
     </div>
   );
